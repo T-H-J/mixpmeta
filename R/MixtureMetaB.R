2b5032ab6a11a5715c3ab3f23a07763ae0cce183
Takahiro Hasegawa <126361607+T-H-J@users.noreply.github.com>
2023-02-28 06:29:26 +0900

    Add files via upload

--- R/MixtureMetaB.R
+++ R/MixtureMetaB.R
@@ -1,91 +1,98 @@
 #' Mixture population model approach for meta analysis of binary outcome
 #'
-#' @param DSN Input data frame
-#' @param WEIGHT Weights used to combine studies: '"size"', '"equal"' or a numeric vector
-#' @param ALPHA Two-sided significance level
-#' @param NBOOT Number of iterations. When default (NBOOT = 2000), 2,000 sets of realizations are generated for calculating confidence intervals of risk ratio estimated by the mixture population model approach.
-#' @param DEMOG A character vector for baseline characteristics included in input data frame
+#' Mixture population model approach to combine information across multiple studies for binary outcomes. This simple and robust procedure based on a mixture population concept can provide a clinically meaningful between-group contrast summary for a well-defined study population.
+#' @param dsn Input data frame with study names in the first column, where variable N0 and N1 are the number of subjects (study size) in control group and treatment group, respectively, and variable R0 and R1 are the number of events in control group and treatment group, respectively.
+#' @param weight Weights used to combine studies: '"size"', '"equal"' or a numeric vector
+#' @param alpha Two-sided significance level
+#' @param nboot Number of iterations. When default (nboot = 2000), 2,000 sets of realizations are generated for calculating confidence intervals of risk ratio estimated by the mixture population model approach.
+#' @param demog A character vector for baseline characteristics included in the input data frame
 #' @returns A data frame
 #' @examples
-#' MixtureMetaB(DSN = EHFE, WEIGHT = "size", DEMOG = c("AGE", "MALE", "BMI", "DD", "FU"))
-#' MixtureMetaB(DSN = EHFE, WEIGHT = "equal")
-#' MixtureMetaB(DSN = EHFE, WEIGHT = rep(1, 6))
+#' MixtureMetaB(dsn = EHFE, weight = "size", demog = c("AGE", "MALE", "BMI", "DD", "FU"))
+#' MixtureMetaB(dsn = EHFE, weight = "equal")
+#' MixtureMetaB(dsn = EHFE, weight = rep(1, 6))
 #' @export
-MixtureMetaB <- function(DSN = NULL, WEIGHT = NULL, ALPHA = 0.05, NBOOT = 2000, DEMOG = NULL){
-  if (tolower(WEIGHT)[1] == "equal") {
-    W <- rep(1 / nrow(DSN), nrow(DSN))
+MixtureMetaB <- function(
+  dsn    = NULL, 
+  weight = NULL, 
+  alpha  = 0.05, 
+  nboot  = 2000, 
+  demog  = NULL
+){
+  if (tolower(weight)[1] == "equal") {
+    W <- rep(1 / nrow(dsn), nrow(dsn))
   } else {
-    if (tolower(WEIGHT)[1] == "size") {
-      W <- with(DSN, (N0 + N1) / sum(N0 + N1))
+    if (tolower(weight)[1] == "size") {
+      W <- with(dsn, (N0 + N1) / sum(N0 + N1))
     } else {
-      if (length(WEIGHT) == nrow(DSN) & is.numeric(WEIGHT)) {
-        W <- WEIGHT / sum(WEIGHT)
+      if (length(weight) == nrow(dsn) & is.numeric(weight)) {
+        W <- weight / sum(weight)
       } else {
         stop(paste("The mixture weight needs to be 'equal', 'size' or a numeric vector with the same length as the number of studies."))
       }
     }
   }
 
-  PR1   <- with(DSN, R1 / N1)
-  PR0   <- with(DSN, R0 / N0)
+  PR1   <- with(dsn, R1 / N1)
+  PR0   <- with(dsn, R0 / N0)
 
   eRR   <- PR1 / PR0
-  vleRR <- with(DSN, (N1 - R1) / R1 / N1 + (N0 - R0) / R0 / N0)
-  eRRl  <- exp(log(eRR) - qnorm(1 - ALPHA / 2) * sqrt(vleRR))
-  eRRu  <- exp(log(eRR) + qnorm(1 - ALPHA / 2) * sqrt(vleRR))
+  vleRR <- with(dsn, (N1 - R1) / R1 / N1 + (N0 - R0) / R0 / N0)
+  eRRl  <- exp(log(eRR) - qnorm(1 - alpha / 2) * sqrt(vleRR))
+  eRRu  <- exp(log(eRR) + qnorm(1 - alpha / 2) * sqrt(vleRR))
 
   eRD   <- PR1 - PR0
-  veRD  <- with(DSN, R1 * (N1 - R1) / N1^3 + R0 * (N0 - R0) / N0^3)
-  eRDl  <- eRD - qnorm(1 - ALPHA / 2) * sqrt(veRD)
-  eRDu  <- eRD + qnorm(1 - ALPHA / 2) * sqrt(veRD)
+  veRD  <- with(dsn, R1 * (N1 - R1) / N1^3 + R0 * (N0 - R0) / N0^3)
+  eRDl  <- eRD - qnorm(1 - alpha / 2) * sqrt(veRD)
+  eRDu  <- eRD + qnorm(1 - alpha / 2) * sqrt(veRD)
 
-  IRES <- with(DSN, matrix(c(PR1, PR0, eRR, eRRl, eRRu, eRD, eRDl, eRDu), ncol = 8))
+  IRES <- with(dsn, matrix(c(PR1, PR0, eRR, eRRl, eRRu, eRD, eRDl, eRDu), ncol = 8))
   IRES <- cbind(IRES, W)
   colnames(IRES) <- c("PR1", "PR0",
-                      "Risk ratio", paste0(round((1 - ALPHA) * 100, 0), "% CI_RR_lower"), paste0(round((1 - ALPHA) * 100, 0), "% CI_RR_upper"),
-                      "Risk difference", paste0(round((1 - ALPHA) * 100, 0), "% CI_RD_lower"), paste0(round((1 - ALPHA) * 100, 0), "% CI_RD_upper"),
-                      "Weights")
+                      "Risk ratio", paste0(round((1 - alpha) * 100, 0), "% CI_RR_lower"), paste0(round((1 - alpha) * 100, 0), "% CI_RR_upper"),
+                      "Risk difference", paste0(round((1 - alpha) * 100, 0), "% CI_RD_lower"), paste0(round((1 - alpha) * 100, 0), "% CI_RD_upper"),
+                      "weights")
 
-  RESULT <- cbind(DSN, IRES)
+  RESULT <- cbind(dsn, IRES)
 
   MP0 <- sum(PR0 * W)
   MP1 <- sum(PR1 * W)
 
   MRR <- MP1 / MP0
 
-  R1B <- matrix(rbinom(length(DSN$R1) * NBOOT, size = DSN$N1, prob = PR1), nrow = length(DSN$R1))
-  R0B <- matrix(rbinom(length(DSN$R0) * NBOOT, size = DSN$N0, prob = PR0), nrow = length(DSN$R0))
+  R1B <- matrix(rbinom(length(dsn$R1) * nboot, size = dsn$N1, prob = PR1), nrow = length(dsn$R1))
+  R0B <- matrix(rbinom(length(dsn$R0) * nboot, size = dsn$N0, prob = PR0), nrow = length(dsn$R0))
 
-  mgRRB <- numeric(NBOOT)
+  mgRRB <- numeric(nboot)
 
-  for (i in 1 : NBOOT){
-    mgRRB[i] <- sum(R1B[, i] / DSN$N1 * W) / sum(R0B[, i] / DSN$N0 * W)
+  for (i in 1:nboot){
+    mgRRB[i] <- sum(R1B[, i] / dsn$N1 * W) / sum(R0B[, i] / dsn$N0 * W)
   }
 
-  MRRlu <- quantile(mgRRB, probs = c(ALPHA / 2, 1 - ALPHA / 2))
+  MRRlu <- quantile(mgRRB, probs = c(alpha / 2, 1 - alpha / 2))
 
   MRD <- MP1 - MP0
-  vMRD <- sum(W^2 * with(DSN, R1 * (N1 - R1) / N1^3)) + sum(W^2 * with(DSN, R0 * (N0 - R0) / N0^3))
-  MRDl <- MRD - qnorm(1 - ALPHA / 2) * sqrt(vMRD)
-  MRDu <- MRD + qnorm(1 - ALPHA / 2) * sqrt(vMRD)
+  vMRD <- sum(W^2 * with(dsn, R1 * (N1 - R1) / N1^3)) + sum(W^2 * with(dsn, R0 * (N0 - R0) / N0^3))
+  MRDl <- MRD - qnorm(1 - alpha / 2) * sqrt(vMRD)
+  MRDu <- MRD + qnorm(1 - alpha / 2) * sqrt(vMRD)
 
-  RESULT <- rbind(RESULT[, -1], rep(NA, ncol(DSN) + 8))
+  RESULT <- rbind(RESULT[, -1], rep(NA, ncol(dsn) + 8))
 
   RESULT$PR1[nrow(RESULT)]  <- MP1
   RESULT$PR0[nrow(RESULT)]  <- MP0
   RESULT[nrow(RESULT), c((ncol(RESULT) - 6) : (ncol(RESULT) - 1))] <- c(MRR, MRRlu, MRD, MRDl, MRDu)
 
-  if (length(DEMOG) > 0){
-    DEMOV <- paste("RESULT", DEMOG, sep = "$")
-    RESV  <- paste0(paste("RESULT", DEMOG, sep = "$"), "[nrow(RESULT)] <- sum(", DEMOV, " * c(W, NA), na.rm = TRUE)")
+  if (length(demog) > 0){
+    DEMOV <- paste("RESULT", demog, sep = "$")
+    RESV  <- paste0(paste("RESULT", demog, sep = "$"), "[nrow(RESULT)] <- sum(", DEMOV, " * c(W, NA), na.rm = TRUE)")
 
-    for (i in 1 : length(DEMOG)){
+    for (i in 1 : length(demog)){
       eval(parse(text = RESV[i]))
     }
   }
 
   NAMES <- colnames(RESULT)
-  RESULT$STUDY <- c(DSN[, 1], paste("Mixture weights =", paste(WEIGHT, collapse = ",")))
+  RESULT$STUDY <- c(dsn[, 1], paste("Mixture weights =", paste(weight, collapse = ",")))
   RESULT <- RESULT[, c("STUDY", NAMES)]
 
   RESULT
--- R/MixtureMetaC.R
+++ R/MixtureMetaC.R
@@ -1,58 +1,65 @@
 #' Mixture population model approach for meta analysis of continuous outcome
 #'
-#' @param DSN Input data frame
-#' @param WEIGHT Weights used to combine studies: '"size"', '"equal"' or a numeric vector
-#' @param ALPHA Two-sided significance level
-#' @param SE 'TRUE' for standard error included in input data frame instead of standard deviation and 'FALSE' (the default) otherwise
-#' @param DEMOG A character vector for baseline characteristics included in input data frame
+#' Mixture population model approach to combine information across multiple studies for continuous outcomes. This simple and robust procedure based on a mixture population concept can provide a clinically meaningful between-group contrast summary for a well-defined study population.
+#' @param dsn Input data frame with study names in the first column, where variable N0 and N1 are the number of subjects (study size) in control group and treatment group, respectively, variable E0 and E1 are the mean of continuous outcome in control group and treatment group, respectively, and variable SD0 and SD1 are the standard deviation of continuous outcome (se = 'FALSE') or the standard error of mean (se = 'TRUE') in control group and treatment group, respectively.
+#' @param weight Weights used to combine studies: '"size"', '"equal"' or a numeric vector
+#' @param alpha Two-sided significance level
+#' @param se 'TRUE' for standard error included in input data frame instead of standard deviation and 'FALSE' (the default) otherwise
+#' @param demog A character vector for baseline characteristics included in input data frame
 #' @returns A data frame
 #' @examples
-#' MixtureMetaC(DSN = ELDL, WEIGHT = "size", DEMOG = c("Age", "Male", "LDL"))
-#' MixtureMetaC(DSN = ELDL, WEIGHT = "equal")
-#' MixtureMetaC(DSN = ELDL, WEIGHT = rep(1, 16))
+#' MixtureMetaC(dsn = ELDL, weight = "size", demog = c("Age", "Male", "LDL"))
+#' MixtureMetaC(dsn = ELDL, weight = "equal")
+#' MixtureMetaC(dsn = ELDL, weight = rep(1, 16))
 #' @export
-MixtureMetaC <- function(DSN = NULL, WEIGHT = NULL, ALPHA = 0.05, SE = FALSE, DEMOG = NULL){
-  if (tolower(WEIGHT)[1] == "equal") {
-    W <- rep(1 / nrow(DSN), nrow(DSN))
+MixtureMetaC <- function(
+  dsn    = NULL, 
+  weight = NULL, 
+  alpha  = 0.05, 
+  se     = FALSE, 
+  demog  = NULL
+){
+  if (tolower(weight)[1] == "equal") {
+    W <- rep(1 / nrow(dsn), nrow(dsn))
   } else {
-    if (tolower(WEIGHT)[1] == "size") {
-      W <- with(DSN, (N0 + N1) / sum(N0 + N1))
+    if (tolower(weight)[1] == "size") {
+      W <- with(dsn, (N0 + N1) / sum(N0 + N1))
     } else {
-      if (length(WEIGHT) == nrow(DSN) & is.numeric(WEIGHT)) {
-        W <- WEIGHT / sum(WEIGHT)
+      if (length(weight) == nrow(dsn) & is.numeric(weight)) {
+        W <- weight / sum(weight)
       } else {
         stop(paste("The mixture weight needs to be 'equal', 'size' or a numeric vector with the same length as the number of studies."))
       }
     }
   }
 
-  if (SE == FALSE) {
-    V0 <- with(DSN, SD0^2 / N0)
-    V1 <- with(DSN, SD1^2 / N1)
+  if (se == FALSE) {
+    V0 <- with(dsn, SD0^2 / N0)
+    V1 <- with(dsn, SD1^2 / N1)
   } else {
-    V0 <- with(DSN, SD0^2)
-    V1 <- with(DSN, SD1^2)
+    V0 <- with(dsn, SD0^2)
+    V1 <- with(dsn, SD1^2)
   }
 
-  IRES <- with(DSN, matrix(c(E1 - E0,
-                             E1 - E0 - qnorm(1 - ALPHA / 2) * sqrt(V0 + V1),
-                             E1 - E0 + qnorm(1 - ALPHA / 2) * sqrt(V0 + V1)), ncol = 3))
+  IRES <- with(dsn, matrix(c(E1 - E0,
+                             E1 - E0 - qnorm(1 - alpha / 2) * sqrt(V0 + V1),
+                             E1 - E0 + qnorm(1 - alpha / 2) * sqrt(V0 + V1)), ncol = 3))
   IRES <- cbind(IRES, W)
-  colnames(IRES) <- c("DIFF", paste0(round((1 - ALPHA) * 100, 0), "% CI_lower"), paste0(round((1 - ALPHA) * 100, 0), "% CI_lower"), "Weights")
+  colnames(IRES) <- c("DIFF", paste0(round((1 - alpha) * 100, 0), "% CI_lower"), paste0(round((1 - alpha) * 100, 0), "% CI_lower"), "weights")
 
-  RESULT <- cbind(DSN, IRES)
+  RESULT <- cbind(dsn, IRES)
 
-  ME0 <- sum(W * DSN$E0)
-  ME1 <- sum(W * DSN$E1)
+  ME0 <- sum(W * dsn$E0)
+  ME1 <- sum(W * dsn$E1)
 
   SE0 <- sqrt(sum(W^2 * V0))
   SE1 <- sqrt(sum(W^2 * V1))
 
   MD  <- ME1 - ME0
-  MDl <- MD - qnorm(1 - ALPHA / 2) * sqrt(SE0^2 + SE1^2)
-  MDu <- MD + qnorm(1 - ALPHA / 2) * sqrt(SE0^2 + SE1^2)
+  MDl <- MD - qnorm(1 - alpha / 2) * sqrt(SE0^2 + SE1^2)
+  MDu <- MD + qnorm(1 - alpha / 2) * sqrt(SE0^2 + SE1^2)
 
-  RESULT <- rbind(RESULT[, -1], rep(NA, ncol(DSN) + 3))
+  RESULT <- rbind(RESULT[, -1], rep(NA, ncol(dsn) + 3))
 
   RESULT$E1[nrow(RESULT)]  <- ME1
   RESULT$SD1[nrow(RESULT)] <- SE1
@@ -60,17 +67,17 @@ MixtureMetaC <- function(DSN = NULL, WEIGHT = NULL, ALPHA = 0.05, SE = FALSE, DE
   RESULT$SD0[nrow(RESULT)] <- SE0
   RESULT[nrow(RESULT), c((ncol(RESULT) - 3) : (ncol(RESULT) - 1))] <- c(MD, MDl, MDu)
 
-  if (length(DEMOG) > 0){
-    DEMOV <- paste("RESULT", DEMOG, sep = "$")
-    RESV  <- paste0(paste("RESULT", DEMOG, sep = "$"), "[nrow(RESULT)] <- sum(", DEMOV, " * c(W, NA), na.rm = TRUE)")
+  if (length(demog) > 0){
+    DEMOV <- paste("RESULT", demog, sep = "$")
+    RESV  <- paste0(paste("RESULT", demog, sep = "$"), "[nrow(RESULT)] <- sum(", DEMOV, " * c(W, NA), na.rm = TRUE)")
 
-    for (i in 1 : length(DEMOG)){
+    for (i in 1:length(demog)){
       eval(parse(text = RESV[i]))
     }
   }
 
   NAMES <- colnames(RESULT)
-  RESULT$STUDY <- c(DSN[, 1], paste("Mixture weights =", paste(WEIGHT, collapse = ",")))
+  RESULT$STUDY <- c(dsn[, 1], paste("Mixture weights =", paste(weight, collapse = ",")))
   RESULT <- RESULT[, c("STUDY", NAMES)]
 
   return(RESULT)
